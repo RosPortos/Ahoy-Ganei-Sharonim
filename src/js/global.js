568fad069a2c3f63e43782e98e2efede6147c938
Rostislav <nehaiki@ukr.net>
2023-01-20 18:00:41 +0200

    add new branch

--- src/js/global.js
+++ src/js/global.js
@@ -18,67 +18,52 @@ document.addEventListener('DOMContentLoaded', function () {
 
     function videoBlock() {
 
-        var player;
-        function createVideo(videoBlockId, videoId) {
-            player = new YT.Player(videoBlockId, {
+        let playButton = document.querySelector(".about-top__video .play-btn");
+        let previeBlock = document.querySelector('.about-top-preview');
+        let videoId = $('#player').attr('data-video-id');
+
+        let playerVideo;
+        window.YT.ready(function () {
+            playerVideo = new YT.Player('player', {
+                height: '360',
+                width: '640',
                 videoId: videoId,
-                playerVars: {
-                    autohide: 1,
-                    showinfo: 0,
-                    rel: 0,
-                    loop: 1,
-                    playsinline: 1,
-                    fs: 1,
-                    allowsInlineMediaPlayback: true,
-                },
                 events: {
-                    onReady: function (e) {
-                        setTimeout(function () {
-                            e.target.playVideo();
-                        }, 200);
-                    },
+                    onReady: onReady,
+                    onStateChange: onStateChange,
                 },
             });
+        });
+
+        function onReady() {
+
+            let iframe = playerVideo.getIframe();
+            let videoTitle = iframe.getAttribute('data-video-title');
+
+            iframe.setAttribute('title', videoTitle);
+            playButton.addEventListener('click', () => {
+                play()
+            });
+
+            function play() {
+                playerVideo.playVideo();
+                previeBlock.classList.add('hide');
+            }
         }
 
-        $("[data-video-modal]").click(function (e) {
-            e.preventDefault();
-            e.stopPropagation();
-
-            let videoId = $(this).data("video-modal");
-            let videoType = $(this).data("video-type");
-
-            if (videoType == "youtube") {
-                $(".about-top__video")
-                    .removeClass("vimeo youtube")
-                    .addClass("youtube")
-                    .append('<div class="video-iframe" id="' + videoId + '"></div>');
-                createVideo(videoId, videoId);
-                $('.about-top-preview').addClass('hide')
-            } else if (videoType == "vimeo") {
-                $(".about-top__video")
-                    .removeClass("vimeo youtube")
-                    .addClass("vimeo")
-                    .html(
-                        '<iframe class="video-iframe" allow="autoplay" src="https://player.vimeo.com/video/' +
-                        videoId +
-                        '?playsinline=1&autoplay=1&transparent=1&app_id=122963">'
-                    );
-                $('.about-top-preview').addClass('hide')
-            } else if (videoType == "mp4" || videoType == "drive") {
-                $(".about-top__video")
-                    .removeClass("vimeo youtube")
-                    .addClass("video")
-                    .html(`
-                        <video controls autoplay playisline>
-                            <source src="${videoId}">
-                        </video>   
-                `);
-                $('.about-top-preview').addClass('hide')
+        function onStateChange(e) {
+            if (e.data == 2) {
+                previeBlock.classList.remove('hide');
             }
-        });
+
+            if (e.data == 0) {
+                previeBlock.classList.remove('hide');
+            }
+
+        }
     }
 
     videoBlock();
+
 
 });
